579ac64b71b5ad2b5f9a6d723d69e52bdeb2de93
Rilder Almeida <49083200+rilder-almeida@users.noreply.github.com>
2021-09-15 06:52:58 -0300

    Update starter_python_project.sh

--- starter_python_project.sh
+++ starter_python_project.sh
@@ -8,8 +8,8 @@ white=$'\e[0m'
 
 
 clear
-echo "${blue}Welcome to Starter Python Project"
-echo "${blue}by Rilder Almeida - https://www.linkedin.com/in/rilder-almeida/${white}"
+echo "${yellow}Welcome to Starter Python Project${white}"
+echo "${yellow}by Rilder Almeida - ${blue}https://www.linkedin.com/in/rilder-almeida/${white}"
 
 installed() {
     return $(dpkg-query -W -f '${Status}\n' "${1}" 2>&1|awk '/ok installed/{print 0;exit}{print 1}')
@@ -28,41 +28,40 @@ if [ ! -z "$missing_pkgs" ]; then
 	while true; do
     read -p "${red}Missing package: $missing_pkgs, confirm to install (y/n)${white}" yn
     case $yn in
-        [Yy]* ) sudo apt install $missing_pkgs && echo "${green}Installed${white}"; break || ( echo "${red}Failed${white}!" && echo "${red}Exiting${white}" && exit 0);;
+        [Yy]* ) sudo apt install $missing_pkgs && echo "${green}Installed${white}"; break || ( echo "${red}Failed${white}!" && echo "${red}Exiting${white}" && exit; );;
         [Nn]* ) exit;;
         * ) echo "${red}Please answer yes or no.${white}";;
     esac
 	done
 fi
 
-read -p "${blue}Insert a name for your new python project: ${white}" project
+read -p "${yellow}Insert a name for your new python project: ${white}" project
 
 if [ -z "$project" ] || [ "$project" = "" ] || [ "$project" = " " ];
-	then echo "${red}Project name must be not null or space${white}"; echo "${red}Exiting${white}"; exit 1
+	then echo "${red}Project name must be not null or space${white}"; echo "${red}Exiting${white}"; exit;
 elif [ -d "$project" ]; then
-	echo "${red}Directory already exist.${white}"; echo "${red}Exiting${white}"; exit 1
+	echo "${red}Directory already exist.${white}"; echo "${red}Exiting${white}"; exit;
 elif [ -d "starter_python_project" ]; then
-	echo "${red}starter_python_project repository already exist.${white}"; echo "${red}Exiting${white}"; exit 1
+	echo "${red}starter_python_project repository already exist.${white}"; echo "${red}Exiting${white}"; exit;
 else
-	echo "${yellow}Repository: https://github.com/rilder-almeida/starter_python_project.git${white}"
-	git clone https://github.com/rilder-almeida/starter_python_project.git || ( echo "${red}Failed${white}!"; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	git clone https://github.com/rilder-almeida/starter_python_project.git || { echo "${red}Failed${white}!"; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1; }
 	
-	echo "${yellow}Creating project folder${white}"
-	mv starter_python_project "$project" && echo "${green}Done!${white}" || ( echo "${red}Failed${white}!"; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	echo "${yellow}Creating project's folder${white}"
+	mv starter_python_project "$project" && echo "${green}Done!${white}" || { echo "${red}Failed${white}!"; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1; }
 	
-	echo "${yellow}Cleaning repository git${white}"
-	cd "$project" && rm -rf .git && rm -rf README.md && rm -rf starter_python_project.sh && echo "${green}Done!${white}" || ( cd ..; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	echo "${yellow}Cleaning the repository${white}"
+	cd "$project" && rm -rf .git && rm -rf README.md && rm -rf starter_python_project.sh && echo "${green}Done!${white}" || { cd ..; rm -rf "$project"; echo "${red}Exiting${white}"; exit 1; }
 	
 	echo "${yellow}Setting README.MD${white}"
-	mv readme_template.md README.MD && echo "${green}Done!${white}" || ( cd ..; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	mv readme_template.md README.MD && echo "${green}Done!${white}" || { cd ..; rm -rf "$project"; echo "${red}Exiting${white}"; exit 1; }
 	
 	echo "${yellow}Initiating git, adding and commiting the repository${white}"
-	git init && git add . && git commit -m "Initial configs" && echo "${green}Done!${white}" || ( cd ..; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	git init && git add . && git commit -m "Initial configs" && echo "${green}Done!${white}" || { cd ..; rm -rf "$project"; echo "${red}Exiting${white}"; exit 1; }
 	
-	echo "${yellow}Initiating Virtual Environment, installing requirements and pre-commit${white}"
-	virtualenv -p python3 venv && . venv/bin/activate && venv/bin/pip3 install -r requirements.txt && pre-commit install && echo "${green}Done!${white}" || ( cd ..; rm -rf starter_python_project; echo "${red}Exiting${white}"; exit 1)
+	echo "${yellow}Initiating Virtual Environment and installing all requirements${white}"
+	virtualenv -p python3 venv && . venv/bin/activate && venv/bin/pip3 install -r requirements.txt && pre-commit install && echo "${green}Done!${white}" || { cd ..; rm -rf "$project"; echo "${red}Exiting${white}"; exit 1; }
 fi
 
-echo "${green}Successful!${white}"
-echo "${yellow}Remember to associate the created virtual environment to project's interpreter!${white}"
-echo "${blue}Enjoy your python project "$project" with good pratices!${white}"
+echo "${green}All Successful!${white}"
+echo "${green}Remember to associate the created virtual environment to project's interpreter!${white}"
+echo "${yellow}Leave a star -> https://github.com/rilder-almeida/starter_python_project.git${white}"
