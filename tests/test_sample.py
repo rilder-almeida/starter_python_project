79b1152167b6c8951909e27e634d8af08b7ad30c
Rilder Almeida <49083200+rilder-almeida@users.noreply.github.com>
2021-09-15 07:56:34 -0300

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,11 @@
 from .context import sample
 from sample import sample_sum_func
+from pytest import mark
 
 
-def test_sample_sum_func():
+@mark.parametrize("num_a,num_b,expected", [(1, 2, 3), (-3, 4, 1), (-1.7, 0.6, -1.1)])
+def test_sample_sum_func(num_a, num_b, expected):
     """testing sum_func"""
-    result = sample_sum_func(1, 2)
-    assert result == 3
+    result = sample_sum_func(num_a, num_b)
+    assert result == expected
+    
